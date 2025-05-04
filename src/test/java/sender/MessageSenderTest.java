7d08f9d7a6852e761152a72c53887c8e5351959e
Anna Zhevlakova <azevlakova12@gmail.com>
2025-05-04 18:48:56 +0400

    Fix after review

--- src/test/java/sender/MessageSenderTest.java
+++ src/test/java/sender/MessageSenderTest.java
@@ -4,8 +4,13 @@ import mocks.GeoServiceMock;
 import mocks.LocalizationServiceMock;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import ru.netology.entity.Country;
+import ru.netology.entity.Location;
 import ru.netology.geo.GeoService;
+import ru.netology.geo.GeoServiceImpl;
 import ru.netology.i18n.LocalizationService;
+import ru.netology.i18n.LocalizationServiceImpl;
 import ru.netology.sender.MessageSenderImpl;
 
 import java.util.HashMap;
@@ -14,8 +19,13 @@ import java.util.Map;
 public class MessageSenderTest {
     @Test
     public void test_send_1() {
-        LocalizationService localizationServiceMock = new LocalizationServiceMock();
-        GeoService geoServiceMock = new GeoServiceMock();
+
+        LocalizationService localizationServiceMock  = Mockito.mock(LocalizationServiceImpl.class);
+        GeoService geoServiceMock = Mockito.mock(GeoServiceImpl.class);
+        Mockito.when(localizationServiceMock.locale(Country.RUSSIA)).thenReturn("Добро пожаловать");
+        Mockito.when(localizationServiceMock.locale(Country.USA)).thenReturn("Welcome");
+        Mockito.when(geoServiceMock.byIp("172.0.32.11")).thenReturn(new Location("Moscow", Country.RUSSIA, "Lenina", 15));
+        Mockito.when(geoServiceMock.byIp("96.44.183.149")).thenReturn(new Location("New York", Country.USA, " 10th Avenue", 32));
         MessageSenderImpl messageSender = new MessageSenderImpl(geoServiceMock, localizationServiceMock);
         Map<String, String> states = new HashMap<String, String>();
         states.put(MessageSenderImpl.IP_ADDRESS_HEADER, "172.0.32.11");
